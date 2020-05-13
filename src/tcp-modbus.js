5254d42629748fa0cc663f1824e32896a56f626a
Michal Molawka <michal.molawka@gmail.com>
2020-05-13 23:37:57 +0200

    added TcpModbus class wrapping new jsmodbus lib

---
+++ src/tcp-modbus.js
@@ -0,0 +1,44 @@
+const jsmodbus = require('jsmodbus')
+const net = require('net')
+const EventEmitter = require('events')
+
+class TcpModbus extends EventEmitter {
+	constructor(host, port) {
+		super()
+
+		this.host = host
+		this.port = port
+		this.socket = new net.Socket()
+		this.client = new jsmodbus.client.TCP(this.socket)
+
+		this.socket.on('connect', () => {
+			this.emit('connect')
+		})
+
+		this.socket.on('error', err => {
+			this.emit('error', err)
+		})
+
+		this._bindClientMethod('readCoils')
+		this._bindClientMethod('writeMultipleCoils')
+		this._bindClientMethod('readDiscreteInputs')
+		this._bindClientMethod('readInputRegisters')
+		this._bindClientMethod('readHoldingRegisters')
+		this._bindClientMethod('writeMultipleRegisters')
+
+		//this.readInputRegisters = this.client.readInputRegisters.bind(this.client)
+	}
+
+	connect() {
+		this.socket.connect({
+			hose: this.host,
+			port: Number(this.port),
+		})
+	}
+
+	_bindClientMethod(methodName) {
+		this[methodName] = this.client[methodName].bind(this.client)
+	}
+}
+
+module.exports = TcpModbus
